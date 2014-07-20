5c6524c2fcfbb3cc571156fb606e1a9e1f8526a3
erolerten <erolerten@ThinkPad-T440s>
2014-07-20 22:10:15 +0300

    repositories for contact, customer and correspondence are added

---
+++ src/java/com/godoro/crm/repository/CorrespondenceRepository.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.godoro.crm.repository;
+
+import com.godoro.crm.entity.Correspondence;
+import com.godoro.library.database.BaseRepository;
+
+/**
+ *
+ * @author erolerten
+ */
+public class CorrespondenceRepository extends BaseRepository<Correspondence>{
+
+    public CorrespondenceRepository (){
+        super(Correspondence.class);
+    }
+}
