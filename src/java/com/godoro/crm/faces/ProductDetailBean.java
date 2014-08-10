d62dc40dacfd5cde6ddc7cb6249d3769adf4025e
erolerten <erolerten@ThinkPad-T440s>
2014-08-10 19:09:11 +0300

    added: product detail bean project repo

--- src/java/com/godoro/crm/faces/ProductDetailBean.java
+++ src/java/com/godoro/crm/faces/ProductDetailBean.java
@@ -9,9 +9,12 @@ package com.godoro.crm.faces;
 import com.godoro.crm.entity.Customer;
 import com.godoro.crm.entity.Employee;
 import com.godoro.crm.entity.Product;
+import com.godoro.crm.repository.ProductRepository;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  *
@@ -31,40 +34,44 @@ public class ProductDetailBean {
      * Creates a new instance of ProductDetailBean
      */
     public ProductDetailBean() {
-        
-        
-    }
-
-    public List<Customer> getCustomerList() {
-        return customerList;
-    }
-
-    public void setCustomerList(List<Customer> customerList) {
-        this.customerList = customerList;
-    }
-
-    public List<Employee> getEmployeeList() {
-        return employeeList;
-    }
-
-    public void setEmployeeList(List<Employee> employeeList) {
-        this.employeeList = employeeList;
-    }
-
-    public long getSelectedCustomerId() {
-        return selectedCustomerId;
-    }
-
-    public void setSelectedCustomerId(long selectedCustomerId) {
-        this.selectedCustomerId = selectedCustomerId;
-    }
-
-    public long getSelectedEmployeeId() {
-        return selectedEmployeeId;
-    }
-
-    public void setSelectedEmployeeId(long selectedEmployeeId) {
-        this.selectedEmployeeId = selectedEmployeeId;
-    }
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
     
+        long productId = 0;
+        if (request.getParameter("productId") != null) {
+            productId = Long.parseLong(request.getParameter("productId"));
+        }else{
+            if (productId == 0) {
+            product = new Product();
+
+        } else {
+            ProductRepository productRepository = new ProductRepository();
+            product = productRepository.find(productId);
+            productRepository.close();
+        }
+        }
+        
+            ProductRepository productRepository = new ProductRepository();
+            product = productRepository.find(productId);
+            productRepository.close();
+        
+        
+    }
+
+    
+    public void save() {
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+
+        long productId = 0;
+        if (request.getParameter("productId") != null) {
+            productId = Long.parseLong(request.getParameter("productId"));
+        }
+        
+        ProductRepository productRepository=new ProductRepository();
+        if(productId==0){
+            productRepository.persist(product);
+        }else{
+            productRepository.merge(product);
+        }
+        productRepository.close();
+    }
 }
---
+++ src/java/com/godoro/crm/repository/ProjectRepository.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.godoro.crm.repository;
+
+import com.godoro.crm.entity.Project;
+import com.godoro.library.database.BaseRepository;
+
+/**
+ *
+ * @author erolerten
+ */
+public class ProjectRepository extends BaseRepository<Project>{
+    
+    public ProjectRepository (){
+        super(Project.class);
+    }
+        
+    
+    
+    
+}
