914b1c7419feb9b0c8745f124e5b482c2bd1e8b1
erolerten <erolerten@ThinkPad-T440s>
2014-09-07 12:51:28 +0300

    contact summary bean yapmaya basladim ama bir konuda takildim hocaya email gonderdim cevap bekliyorum

---
+++ src/java/com/godoro/crm/faces/ContactSummaryBean.java
@@ -0,0 +1,166 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.godoro.crm.faces;
+
+import com.godoro.crm.entity.Contact;
+import com.godoro.crm.entity.Correspondence;
+import com.godoro.crm.entity.Customer;
+import com.godoro.crm.entity.HashTag;
+import com.godoro.crm.entity.Participant;
+import com.godoro.crm.repository.ContactRepository;
+import com.godoro.crm.repository.CorrespondenceRepository;
+import com.godoro.crm.repository.CustomerRepository;
+import com.godoro.crm.repository.HashTagRepository;
+import com.godoro.crm.repository.ParticipantRepository;
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author erolerten
+ */
+@ManagedBean
+@RequestScoped
+public class ContactSummaryBean {
+    
+    private List<Contact> contactList;
+    private List<Customer> customerList;
+    private long selectedCustomerId;
+    private List<HashTag> hashTagList;
+    private long selectedHashTagId;
+    private List<Correspondence> correspondenceList;
+    private long selectedCorrespondenceId;
+    private List<Participant> participantList;
+    private long selectedParticipantId;
+
+    public List<Contact> getContactList() {
+        return contactList;
+    }
+
+    public void setContactList(List<Contact> contactList) {
+        this.contactList = contactList;
+    }
+    
+    
+
+    public List<Customer> getCustomerList() {
+        return customerList;
+    }
+
+    public void setCustomerList(List<Customer> customerList) {
+        this.customerList = customerList;
+    }
+
+    public long getSelectedcustomerId() {
+        return selectedCustomerId;
+    }
+
+    public void setSelectedcustomerId(long selectedcustomerId) {
+        this.selectedCustomerId = selectedcustomerId;
+    }
+
+    public List<HashTag> getHashTagList() {
+        return hashTagList;
+    }
+
+    public void setHashTagList(List<HashTag> hashTagList) {
+        this.hashTagList = hashTagList;
+    }
+
+    public long getSelectedHashTagId() {
+        return selectedHashTagId;
+    }
+
+    public void setSelectedHashTagId(long selectedHashTagId) {
+        this.selectedHashTagId = selectedHashTagId;
+    }
+
+    public List<Correspondence> getCorrespondenceList() {
+        return correspondenceList;
+    }
+
+    public void setCorrespondenceList(List<Correspondence> correspondenceList) {
+        this.correspondenceList = correspondenceList;
+    }
+
+    public long getSelectedCorrespondenceId() {
+        return selectedCorrespondenceId;
+    }
+
+    public void setSelectedCorrespondenceId(long selectedCorrespondenceId) {
+        this.selectedCorrespondenceId = selectedCorrespondenceId;
+    }
+
+    public List<Participant> getParticipantList() {
+        return participantList;
+    }
+
+    public void setParticipantList(List<Participant> participantList) {
+        this.participantList = participantList;
+    }
+
+    public long getSelectedParticipantId() {
+        return selectedParticipantId;
+    }
+
+    public void setSelectedParticipantId(long selectedParticipantId) {
+        this.selectedParticipantId = selectedParticipantId;
+    }
+    
+    
+    /**
+     * Creates a new instance of ContactSummaryBean
+     */
+    public ContactSummaryBean() {
+        
+        ContactRepository contactRepository = new ContactRepository();
+        contactList = contactRepository.list();
+        contactRepository.close();
+
+        CustomerRepository customerRepository = new CustomerRepository();
+        customerList = customerRepository.list();
+        customerRepository.close();
+
+        HashTagRepository hashTagRepository = new HashTagRepository();
+        hashTagList = hashTagRepository.list();
+        hashTagRepository.close();
+        
+        CorrespondenceRepository correspondenceRepository = new CorrespondenceRepository();
+        correspondenceList = correspondenceRepository.list();
+        correspondenceRepository.close();
+        
+        ParticipantRepository participantRepository = new ParticipantRepository();
+        participantList = participantRepository.list();
+        participantRepository.close();
+        
+    }
+    
+    
+    public void filter() {
+        System.out.println("Secilen Okul kimligi " + selectedCustomerId);
+        if (selectedCustomerId != 0) {
+            ContactRepository contactRepository = new ContactRepository();
+            contactList = contactRepository.listByCustomerId(selectedCustomerId);
+            contactRepository.close();
+
+        } else {
+            if (selectedRoomId != 0) {
+                ContactRepository contactRepository = new ContactRepository();
+                contactList = contactRepository.listByRoomId(selectedRoomId);
+                contactRepository.close();
+            } else {
+
+                ContactRepository contactRepository = new ContactRepository();
+                contactList = contactRepository.list();
+                contactRepository.close();
+            }
+        }
+
+    }
+    
+}
